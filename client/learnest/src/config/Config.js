b1e7c84e7e164d77178ebabdd435737b1f5199e0
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2022-12-04 16:14:23 +0530

    Update env

--- client/learnest/src/config/Config.js
+++ client/learnest/src/config/Config.js
@@ -1,9 +1,9 @@
 const environment = {
-    "GET_CUSTOM_UPLOAD_TOKEN": "http://localhost:5000/api/v1/get/custom/upload/token",
+    "GET_CUSTOM_UPLOAD_TOKEN": "http://localhost:4000/api/v1/get/custom/upload/token",
     "API_VIDEO_BASE": "https://ws.api.video/videos",
-    "SAVE_COURSE": "http://localhost:5000/api/v1/save/course",
-    "CREATE_ORDER": "http://localhost:5000/api/v1/create/order",
-    "VERIFY_PAYMENT": "http://localhost:5000/api/v1/verify/payment",
+    "SAVE_COURSE": "http://localhost:4000/api/v1/save/course",
+    "CREATE_ORDER": "http://localhost:4000/api/v1/create/order",
+    "VERIFY_PAYMENT": "http://localhost:4000/api/v1/verify/payment",
     "RAZORPAY_CHECKOUT": "https://checkout.razorpay.com/v1/checkout.js"
 }
 
