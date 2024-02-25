8d20fe18dc572821fef22603b40595ebd80b4583
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2024-02-25 10:11:11 +0530

    Add code formatting rules

---
+++ client/learnest/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        bracketSpacing: true,
+        eslintIntegration: true,
+        printWidth: 120,
+        jsxBracketSameLine: true,
+        jsxSingleQuote: true,
+        arrowParens: 'always',
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+  },
+}
