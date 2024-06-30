b905e27856a2d9c474e9f9591c7d92797e73e42c
abhishekp6 <53012981+abhishekp6@users.noreply.github.com>
2024-06-30 12:51:34 +0530

    dev env

--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -1,14 +1,14 @@
 const environment = {
-  GET_CUSTOM_UPLOAD_TOKEN: 'https://learnest-be.onrender.com/api/v1/get/custom/upload/token',
+  GET_CUSTOM_UPLOAD_TOKEN: 'http://localhost:4000/api/v1/get/custom/upload/token',
   API_VIDEO_BASE: 'https://ws.api.video/videos',
-  CREATE_ORDER: 'https://learnest-be.onrender.com/api/v1/create/order',
-  VERIFY_PAYMENT: 'https://learnest-be.onrender.com/api/v1/verify/payment',
+  CREATE_ORDER: 'http://localhost:4000/api/v1/create/order',
+  VERIFY_PAYMENT: 'http://localhost:4000/api/v1/verify/payment',
   RAZORPAY_CHECKOUT: 'https://checkout.razorpay.com/v1/checkout.js',
-  GET_COURSE_BY_ID: 'https://learnest-be.onrender.com/api/v1/get/courseById/',
-  SAVE_COURSE: 'https://learnest-be.onrender.com/api/v1/save/course',
-  UPDATE_COURSE: 'https://learnest-be.onrender.com/api/v1/update/course',
-  DELETE_COURSE: 'https://learnest-be.onrender.com/api/v1/delete/course/',
-  GET_ALL_COURSE: 'https://learnest-be.onrender.com/api/v1/fetch/all/course',
+  GET_COURSE_BY_ID: 'http://localhost:4000/api/v1/get/courseById/',
+  SAVE_COURSE: 'http://localhost:4000/api/v1/save/course',
+  UPDATE_COURSE: 'http://localhost:4000/api/v1/update/course',
+  DELETE_COURSE: 'http://localhost:4000/api/v1/delete/course/',
+  GET_ALL_COURSE: 'http://localhost:4000/api/v1/fetch/all/course',
   GOOGLE_USER_INFO: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=',
 }
 
